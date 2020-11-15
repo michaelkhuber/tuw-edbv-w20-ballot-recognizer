9a84a2ed54b56653cf7e4597d40f10de3019cde9
emtiebb <marie_th.wiedhalm@me.com>
2020-11-15 12:43:58 +0100

    Update Circles.m

--- Project/Circles.m
+++ Project/Circles.m
@@ -1,9 +1,9 @@
 function ballotCircles = Circles(ballot)
 % TODO - Implement this function
-ballotCircles = {};
-    for i = [1,2,3,4,5,6,7,8,9]
-        ballotCircles{i} = imread("resources/circle_example.png");
-    end
+%ballotCircles = {};
+%   for i = [1,2,3,4,5,6,7,8,9]
+%      ballotCircles{i} = imread("resources/circle_example.png");
+% end
 
 %I = rgb2gray(ballot);
 %imshow(I);
