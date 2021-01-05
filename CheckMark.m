548da34dc8d2d3d7346032b3b93b67fa96c62458
markush97 <markus@kodas.io>
2021-01-05 01:21:11 +0100

    removed 3rd detection way

--- CheckMark.m
+++ CheckMark.m
@@ -28,25 +28,25 @@ function markedCircleIndices = CheckMark(ballotCircles)
         else
            % split horizontally & count components, if there is only one 
            % empty circle, it should stay with 1 component          
-           for j=scan_dpixel: (size(circle, 1) / scan_dpixel)
-               tmp_circle = circle;
-               tmp_circle(2:end-1, j * scan_dpixel) = 0;
-               [num_components, ~, ~] = CountComponents(tmp_circle);
+           %for j=scan_dpixel: (size(circle, 1) / scan_dpixel)
+           %    tmp_circle = circle;
+           %    tmp_circle(2:end-1, j * scan_dpixel) = 0;
+           %    [num_components, ~, ~] = CountComponents(tmp_circle);
                
-               if (num_components > 1) 
-                  markedCircleIndices(end+1) = k;
-                  break;
-               end
+           %    if (num_components > 1) 
+           %       markedCircleIndices(end+1) = k;
+           %       break;
+           %    end
                
-               tmp_circle = circle;
-               tmp_circle(j * scan_dpixel, 2:end-1) = 0;
-               [num_components, ~, ~] = CountComponents(tmp_circle);
-               
-               if (num_components > 1) 
-                  markedCircleIndices(end+1) = k;
-                  break;
-               end
-           end          
+           %    tmp_circle = circle;
+           %    tmp_circle(j * scan_dpixel, 2:end-1) = 0;
+           %    [num_components, ~, ~] = CountComponents(tmp_circle);
+           %    
+           %    if (num_components > 1) 
+           %       markedCircleIndices(end+1) = k;
+           %       break;
+           %    end
+           %end          
         end
     end
 end
