34577eb8ad4eb2b6a1c4250562af5ed4a3335f9c
emtiebb <marie_th.wiedhalm@me.com>
2021-01-05 12:33:04 +0100

    Update toGray.m

--- assisting_functions/toGray.m
+++ assisting_functions/toGray.m
@@ -17,6 +17,6 @@ function [gray] = toGray(img)
     if c==3 
         gray = 0.2989 * img(:,:,1) + 0.5870 * img(:,:,2) + 0.1140 * img(:,:,3);
     else
-        gray=A;
+        gray=img;
     end
 end
