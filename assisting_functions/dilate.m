ec8656ec98d8766ec8dac13b62a4a3281351745d
emtiebb <marie_th.wiedhalm@me.com>
2021-01-07 22:34:45 +0100

    Update dilate.m

--- assisting_functions/dilate.m
+++ assisting_functions/dilate.m
@@ -14,20 +14,5 @@
 %   dImg:                      dilateted image  
 
 function [dImg] = dilate(img, mask)
-%     [ai,bi]=size(img);
-%     [am,bm]=size(mask);
-%     deltaa = floor(am/2);
-%     deltab = floor(bm/2);
-%     out = img;
-% 
-%     for i = ceil(am/2):ai-deltaa
-%         for j = ceil(bm/2):bi-deltab
-%             fenster = img(i-deltaa:i+deltaa,j-deltab:j+deltab);
-%             fenster = fenster(logical(mask));
-%             out(i,j) = max(fenster);
-%         end
-%     end
-%     dImg = out;
-
     dImg = ordfilt2(img, length(find(mask)), mask);
 end
